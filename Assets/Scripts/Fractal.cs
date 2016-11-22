8b1025229df7580df53e011e13fca389dbafa31e
Du Yue <duyue36@gmail.com>
2016-11-22 10:48:11 +0100

    no sense

---
+++ Assets/Scripts/Fractal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+    public Mesh mesh;
+    public Material material;
+
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
