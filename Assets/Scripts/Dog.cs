73520e53f33b53b851e67163f99f2d58026edc6c
Du Yue <duyue36@gmail.com>
2016-11-22 10:51:11 +0100

    test the update
    Dog is coming

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dog : MonoBehaviour {
+
+    public string name;
+    public int age;
+
+    private void Start()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
